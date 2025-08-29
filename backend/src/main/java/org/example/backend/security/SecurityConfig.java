baf8ca05ade07485992e91af664c3411aa79b43b
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-08-29 13:18:50 +0200

    Basic Security Config without CSRF should be edited later.

---
+++ backend/src/main/java/org/example/backend/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package org.example.backend.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Value("${app.url}")
+    String appUrl;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(a -> a
+                        .requestMatchers("/api/admin").hasAuthority("ADMIN")
+                        .anyRequest().permitAll())
+                .oauth2Login(o -> o
+                        .defaultSuccessUrl(appUrl+"admin"));
+
+        return http.build();
+    }
+}
