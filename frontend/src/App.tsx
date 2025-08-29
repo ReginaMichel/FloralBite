0f7bb463cdb5984f126ed51d38a2540ee95e4bfc
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-08-29 14:02:18 +0200

    added AdminPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
-import './App.css'
-import PlaceHolderPage from "./pages/PlaceholderPage.tsx"
+import './App.css';
+import PlaceHolderPage from "./pages/PlaceholderPage.tsx";
+import AdminPage from "./pages/AdminPage.tsx";
 import {Route, Routes} from "react-router-dom";
 
 function App() {
@@ -7,6 +8,8 @@ function App() {
   return (
     <Routes>
       <Route path="" element={<PlaceHolderPage />}/>
+
+      <Route path="/admin" element={<AdminPage />}/>
     </Routes>
   )
 }
