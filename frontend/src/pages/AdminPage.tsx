eadb444531182ad495c89d215c2b8e2d5f6686b5
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-08-29 14:04:04 +0200

    added AdminPage

--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -5,7 +5,7 @@ export default function AdminPage() {
             <img src={"/assets/placeholderLogo.png"} alt={"Vorläufiges Logo von Floral Bite"} className={"placeholder"}/>
             <img src={"/assets/placeholderTitle.png"} alt={"Vorläufiger Schriftzug von Floral Bite"} className={"placeholder"} width={"50%"}/>
             <h1 className={"placeholder"}>Hallo du!</h1>
-            <h2 className={"placeholder"}>Du bist erfolgreich auf der Admin-Page gelandet.</h2>
+            <h2 className={"placeholder"}>Du bist erfolgreich auf der Admin-Seite gelandet.</h2>
         </div>
     );
 }
