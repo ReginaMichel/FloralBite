3d73a8745b162bedd1be03f4ddd0f1e964fc9b2c
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-08-29 09:57:58 +0200

    added some basic css for placeholder page

--- frontend/src/pages/PlaceholderPage.tsx
+++ frontend/src/pages/PlaceholderPage.tsx
@@ -1,11 +1,11 @@
 export default function PlaceholderPage() {
 
     return (
-        <div className="centered">
-            <img src={"/assets/placeholderLogo.png"} alt={"Vorläufiges Logo von Floral Bite"}/>
-            <img src={"/assets/placeholderTitle.png"} alt={"Vorläufiger Schriftzug von Floral Bite"} width={"50%"}/>
-            <h1>Herzlich Willkommen bei Floral Bite!</h1>
-            <h2>Diese Website befindet sich noch im Aufbau. Bitte wenden Sie sich für Anfragen an Julia Schreck.</h2>
+        <div>
+            <img src={"/assets/placeholderLogo.png"} alt={"Vorläufiges Logo von Floral Bite"} className={"placeholder"}/>
+            <img src={"/assets/placeholderTitle.png"} alt={"Vorläufiger Schriftzug von Floral Bite"} className={"placeholder"} width={"50%"}/>
+            <h1 className={"placeholder"}>Herzlich Willkommen bei Floral Bite!</h1>
+            <h2 className={"placeholder"}>Diese Website befindet sich noch im Aufbau. Bitte wenden Sie sich für Anfragen an Julia Schreck.</h2>
         </div>
     );
 }
