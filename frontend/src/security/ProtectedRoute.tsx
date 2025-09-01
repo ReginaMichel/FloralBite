bfc636f3dd9c41f730adae802150f854b1f292e4
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-01 13:13:41 +0200

    bugfix. Laden des Users wird nun abgewartet und er wird nicht durch Timing-Probleme zurück zur Hauptseite geschickt.

--- frontend/src/security/ProtectedRoute.tsx
+++ frontend/src/security/ProtectedRoute.tsx
@@ -8,7 +8,7 @@ type ProtectedRouteProps = {
 export default function ProtectedRoute(props: Readonly<ProtectedRouteProps>) {
 
     if (props.user === undefined) {
-        <h3>Zugang wird geladen.</h3>
+        return (<h3>Zugang wird geladen...</h3>)
     }
     return (
         props.user?.role === "ADMIN" ? <Outlet/> : <Navigate to = {"/"} />
