9514336fd91a4b7cded8935c6c59f7cc55ea2f89
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-03 15:49:54 +0200

    Cleaner Code

--- frontend/src/styles/themeConfig.ts
+++ frontend/src/styles/themeConfig.ts
@@ -1,47 +1,42 @@
-export const themes = {
-    "/": {
+// Farbthemen
+const baseThemes = {
+    rot: {
         headerBackground: "/assets/header_03.webp",
         background: "#EFCAC8",
         linkColor: "#802F2F",
     },
-    "/about": {
-        headerBackground: "/assets/header_03.webp",
-        background: "#EFCAC8",
-        linkColor: "#802F2F",
-    },
-    "/gallery": {
+    orange1: {
         headerBackground: "/assets/header_02.webp",
         background: "#F6D1AE",
         linkColor: "#A65200",
     },
-    "/admin": {
+    green: {
         headerBackground: "/assets/header_04.webp",
         background: "#7AAB80",
         linkColor: "#254038",
     },
-    "/adminlogin": {
-        headerBackground: "/assets/header_04.webp",
-        background: "#7AAB80",
-        linkColor: "#254038",
-    },
-    "/impressum": {
-        headerBackground: "/assets/header_04.webp",
-        background: "#7AAB80",
-        linkColor: "#254038",
-    },
-    "/contact": {
+    orange2: {
         headerBackground: "/assets/header_01.webp",
         background: "#F6D1AE",
         linkColor: "#A65200",
     },
-    "/offer": {
-        headerBackground: "/assets/header_01.webp",
-        background: "#F6D1AE",
-        linkColor: "#A65200",
-    },
-    default: {
-        headerBackground: "/assets/header_03.webp",
-        background: "#EFCAC8",
-        linkColor: "#802F2F",
-    }
-}
+};
+
+// Zuordnung von Pfaden zu Farbthemen
+const themeMap: Record<string, keyof typeof baseThemes> = {
+    "/": "rot",
+    "/about": "rot",
+    "/gallery": "orange1",
+    "/admin": "green",
+    "/adminlogin": "green",
+    "/impressum": "green",
+    "/contact": "orange2",
+    "/offer": "orange2",
+};
+
+// Gibt die Farbthemen der Pfade zurück
+export const themes: Record<string, typeof baseThemes.rot> = Object.fromEntries(
+    Object.entries(themeMap).map(([path, themeKey]) => [path, baseThemes[themeKey]])
+);
+
+themes.default = baseThemes.rot;
