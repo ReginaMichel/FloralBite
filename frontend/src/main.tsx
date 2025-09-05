eed649d718c35d25418ab7430f096480e7715da8
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-05 16:10:10 +0200

    Die Wunschschriftart Inter von GoogleFonts importiert und lokal eingebunden, damit es keinen DSGVO-kram zu beachten gibt.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,10 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
+// Zentraler Import der Fonts in allen gewünschten Gewichten.
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/700.css'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
