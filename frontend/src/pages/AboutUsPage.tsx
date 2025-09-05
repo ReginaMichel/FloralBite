73f7af11d370ce918f11675655f0913694a6426d
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-05 15:29:06 +0200

    dummy für die about-us-page

--- frontend/src/pages/AboutUsPage.tsx
+++ frontend/src/pages/AboutUsPage.tsx
@@ -1,9 +1,43 @@
-import PlaceholderPage from "./PlaceholderPage.tsx";
-
 export default function AboutUsPage() {
     return (
-        <>
-            <PlaceholderPage/>
-        </>
+        <div className="AboutUsPage">
+            <div className={"rowAlignment"}>
+                <img src={"/assets/PlaceholderJulia.jpg"} alt={"Bild von Julia Schreck"} className={"placeholderJulia"}/>
+                <div className={"textBox"}>
+                    <h1>Hi, ich bin Julia von Floral Bite</h1>
+                    <p className={"leftAlign"}>Ich habe Floral Bite ins Leben gerufen, weil ich wahnsinnig gerne für Menschen koche und backe
+                    und weil mir Tierschutz und Nachhaltigkeit am Herzen liegen. Lorem ipsum dolor sit amet, consetetur
+                        sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+                        erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
+                        kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+                        amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                        magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+                        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
+                        dolor sit amet, consetetur
+                        sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+                        erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
+                        kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+                        amet, consetetur
+                        sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+                        erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
+                        kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                </div>
+            </div>
+            <div>
+                <p className={"leftAlign"}>
+                    Doch besteht Floral Bite nicht nur aus einer Person. Im Hintergrund haben viele Menschen dazu
+                    beigetragen, meinen Traum Realität werden zu lassen. Unterstützung von Familie und Freund*innen bla bla.
+                    Logo und Design der Website wurden von Katharina Konradi geschaffen. Gebaut und betrieben wird die
+                    Seite von Regina Michel. Lorem ipsum dolor sit amet, consetetur
+                    sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+                    erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
+                    kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+                    consetetur
+                    sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+                    erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
+                    kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                </p>
+            </div>
+        </div>
     )
 }
