db7ebdd42e686023b5352a79ebe8f64ea6db0c0c
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-08 19:19:12 +0200

    Google Font "Playfair Display" für Header hinzugefügt.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 // Zentraler Import der Fonts in allen gewünschten Gewichten.
 import '@fontsource/inter/400.css';
 import '@fontsource/inter/700.css'
+import '@fontsource/playfair-display/400.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
