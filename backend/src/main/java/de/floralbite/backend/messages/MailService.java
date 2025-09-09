60cd59518ae139f784aed262d6fc22eb1a58e827
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-09 18:56:23 +0200

    send mail to User

--- backend/src/main/java/de/floralbite/backend/messages/MailService.java
+++ backend/src/main/java/de/floralbite/backend/messages/MailService.java
@@ -1,18 +1,23 @@
 package de.floralbite.backend.messages;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
 
 @Service
 public class MailService {
 
     private final MessageRepo messageRepo;
     private final IdService idService;
+    private final JavaMailSender mailSender;
+    @Value("${HETZNER_USER}")
+    private final String fromAddress;
 
-    public MailService(MessageRepo messageRepo, IdService idService) {
+    public MailService(MessageRepo messageRepo, IdService idService, JavaMailSender mailSender) {
         this.messageRepo = messageRepo;
         this.idService = idService;
+        this.mailSender = mailSender;
     }
 
     public void handleContactMessage(MessageDTO messageDTO) {
@@ -29,7 +34,15 @@ public class MailService {
     }
 
     public void sendMailToUser(Message message) {
-
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(message.email());
+        mailMessage.setSubject("Vielen Dank für deine Nachricht!");
+        mailMessage.setText("Hallo " + message.name() + ",\n\n" +
+                "vielen Dank für deine Nachricht. Ich werde mich so bald wie möglich bei dir melden." + "\n\n" +
+                "Viele Grüße" + "\n" + "Julia von FloralBite" + "\n\n" +
+                "Deine Nachricht:" + "\n" + message.message());
+        mailMessage.setFrom(fromAddress);
+        mailSender.send(mailMessage);
     }
 
     public void addMessage(Message message) {
