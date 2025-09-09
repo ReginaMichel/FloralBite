f60cab0a43d00f891332e5a6ae64920bfd468245
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-09 17:27:16 +0200

    basic service

--- backend/src/main/java/de/floralbite/backend/messages/MailService.java
+++ backend/src/main/java/de/floralbite/backend/messages/MailService.java
@@ -2,12 +2,37 @@ package de.floralbite.backend.messages;
 
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class MailService {
 
     private final MessageRepo messageRepo;
+    private final IdService idService;
 
-    public MailService(MessageRepo messageRepo) {
+    public MailService(MessageRepo messageRepo, IdService idService) {
         this.messageRepo = messageRepo;
+        this.idService = idService;
+    }
+
+    public void handleContactMessage(MessageDTO messageDTO) {
+        String requestId = idService.newId();
+        Message message = new Message(requestId,
+                messageDTO.name(), messageDTO.subject(), messageDTO.email(), messageDTO.phoneNumber(), messageDTO.message());
+        this.sendMailToOwner(message);
+        this.sendMailToUser(message);
+        this.addMessage(message);
+    }
+
+    public void sendMailToOwner(Message message) {
+
+    }
+
+    public void sendMailToUser(Message message) {
+
+    }
+
+    public void addMessage(Message message) {
+        messageRepo.save(message);
     }
 }
