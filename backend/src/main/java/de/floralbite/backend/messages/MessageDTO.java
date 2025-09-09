ece5f2a7380f3ba506369c7d6eaeacd4db5bc34a
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-09 17:09:01 +0200

    added name for mongodb

--- backend/src/main/java/de/floralbite/backend/messages/MessageDTO.java
+++ backend/src/main/java/de/floralbite/backend/messages/MessageDTO.java
@@ -1,4 +1,7 @@
 package de.floralbite.backend.messages;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "contactMessages")
 public record MessageDTO(String name, String subject, String email, String phoneNumber, String message) {
 }
