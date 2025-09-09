1a7ffd81deb7aa624ca4420c11a6be9104e5ecfb
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-09 17:15:08 +0200

    added Message for MessageDTO

--- backend/src/main/java/de/floralbite/backend/messages/MessageDTO.java
+++ backend/src/main/java/de/floralbite/backend/messages/MessageDTO.java
@@ -1,7 +1,4 @@
 package de.floralbite.backend.messages;
 
-import org.springframework.data.mongodb.core.mapping.Document;
-
-@Document(collection = "contactMessages")
 public record MessageDTO(String name, String subject, String email, String phoneNumber, String message) {
 }
