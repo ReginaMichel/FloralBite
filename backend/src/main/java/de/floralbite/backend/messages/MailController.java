53bd8dfb7e80c0f4010c69953cc82190b71e45f6
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-09 17:06:06 +0200

    postmapping endpunkt im api controller

--- backend/src/main/java/de/floralbite/backend/messages/MailController.java
+++ backend/src/main/java/de/floralbite/backend/messages/MailController.java
@@ -1,5 +1,8 @@
 package de.floralbite.backend.messages;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +14,12 @@ public class MailController {
 
     public MailController(MailService mailService) {
         this.mailService = mailService;
+    }
+
+    // Leitet die Message an den Service weiter, und returned ohne Rückgabe Inhalt, die HTTP-Message 200 Ok.
+    @PostMapping
+    public ResponseEntity<Void> handleContactMessage(@RequestBody MessageDTO message) {
+        mailService.handleContactMessage(message);
+        return ResponseEntity.ok().build();
     }
 }
