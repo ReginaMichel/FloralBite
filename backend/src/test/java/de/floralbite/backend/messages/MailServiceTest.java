2156dc2ca09f9f713efa5fa68ceeecfd11e5db56
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-10 10:49:05 +0200

    unit test for handle contact message

--- backend/src/test/java/de/floralbite/backend/messages/MailServiceTest.java
+++ backend/src/test/java/de/floralbite/backend/messages/MailServiceTest.java
@@ -2,6 +2,7 @@ package de.floralbite.backend.messages;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
 import static org.mockito.Mockito.*;
@@ -12,6 +13,7 @@ class MailServiceTest {
     private IdService idService;
     private MailService mailService;
     private Message message;
+    private MessageDTO messageDTO;
     private JavaMailSender mailSender;
 
     @BeforeEach
@@ -20,19 +22,31 @@ class MailServiceTest {
         this.idService = mock(IdService.class);
         this.mailSender = mock(JavaMailSender.class);
         this.mailService = new MailService(repo, idService, mailSender);
-        this.message = new Message("3","Karen","feedback","karen@mail.de","0123","Hi");
+        this.message = new Message("1","Karen","feedback","karen@mail.de","0123","Hi");
+        this.messageDTO = new MessageDTO("Karen","feedback","karen@mail.de","0123","Hi");
     }
 
     @Test
     void addMessage() {
-        //GIVEN
+        // GIVEN
         when(repo.save(message)).thenReturn(message);
-
         // WHEN
         mailService.addMessage(message);
-
         // THEN
         verify(repo, times(1)).save(any(Message.class));
-        verify(repo, never()).findById("3");
+        verify(repo, never()).findById("1");
+    }
+
+    @Test
+    void handleContactMessage() {
+        // GIVEN
+        when(idService.newId()).thenReturn("1");
+        when(repo.save(any(Message.class))).thenReturn(message);
+        // WHEN
+        mailService.handleContactMessage(messageDTO);
+        // THEN
+        verify(idService, times(1)).newId();
+        verify(mailSender, times(2)).send(any(SimpleMailMessage.class));
+        verify(repo, times(1)).save(any(Message.class));
     }
 }
