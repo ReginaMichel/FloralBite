ab27df429136691472bb5bb0ba1284a8ba7f7fd6
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-15 20:57:21 +0200

    text

--- frontend/src/pages/AboutUsPage.tsx
+++ frontend/src/pages/AboutUsPage.tsx
@@ -13,7 +13,7 @@ export default function AboutUsPage() {
                 </Grid>
                 <Grid size={{xs: 12, sm: 7}}>
                     <h2>Hi, ich bin Julia</h2>
-                    <p>Ich habe FloralBite ins Leben gerufen, weil ich wahnsinnig gerne für Menschen koche und backe
+                    <p>Ich habe FloralBite ins Leben gerufen, weil ich gerne für Menschen koche und backe
                         und weil mir Tierschutz und Nachhaltigkeit am Herzen liegen.</p>
                     <p>Im Hintergrund tragen viele Menschen dazu bei, meinen Traum Wirklichkeit werden zu lassen.
                         Logo und Design wurden von Katharina Konradi geschaffen. Die Webseite wird von Regina Michel
