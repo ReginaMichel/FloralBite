48468e93fa4744f443052de60c777f50ce5dedb0
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-15 19:56:33 +0200

    farbe geändert, damit das gesamt design besser matcht.

--- frontend/src/styles/themeConfig.ts
+++ frontend/src/styles/themeConfig.ts
@@ -3,22 +3,22 @@ const baseThemes = {
     rot: {
         headerBackground: "/assets/header_03.webp",
         background: "239, 202, 200",   // #EFCAC8
-        linkColor: "128, 47, 47",      // #802F2F
+        linkColor: "17, 45, 33" //"128, 47, 47",      // #802F2F
     },
     orange1: {
         headerBackground: "/assets/header_02.webp",
         background: "246, 209, 174",   // #F6D1AE
-        linkColor: "166, 82, 0",       // #A65200
+        linkColor: "17, 45, 33" //"166, 82, 0",       // #A65200
     },
     green: {
         headerBackground: "/assets/header_04.webp",
         background: "122, 171, 128",   // #7AAB80
-        linkColor: "37, 64, 56",       // #254038
+        linkColor: "17, 45, 33" //"37, 64, 56",       // #254038
     },
     orange2: {
         headerBackground: "/assets/header_01.webp",
         background: "255, 225, 164",   // background: #FFE1A4;
-        linkColor: "166, 82, 0",       //
+        linkColor: "17, 45, 33" //"166, 82, 0",       //
     },
 };
 
