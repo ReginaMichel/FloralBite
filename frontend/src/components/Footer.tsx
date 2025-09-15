88947711a632a9a66ed279f945394ebaee48fe1e
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-15 18:28:26 +0200

    typo

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
                 <Link to={"/kontakt"}>Kontakt</Link>
             </div>
             <div className={"right"}>
-                <Link to={"/datenschutz"}>Impressum</Link>
+                <Link to={"/datenschutz"}>Datenschutz</Link>
                 <Link to={"/impressum"}>Impressum</Link>
             </div>
         </footer>
