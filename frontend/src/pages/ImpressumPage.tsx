1c30fdf5fac5c766490859fdafad49a52c224878
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-16 19:28:37 +0200

    korrektur

--- frontend/src/pages/ImpressumPage.tsx
+++ frontend/src/pages/ImpressumPage.tsx
@@ -3,7 +3,7 @@ export default function ImpressumPage() {
     return (
         <div className={"textCentered"}>
             <div className="note">
-                <h1>Hinweis</h1>
+                <h2>Hinweis</h2>
                 <p>Floral Bite und diese Website befinden sich noch im Aufbau. Es werden keine Dienstleistungen angeboten.
                     Falls Sie Fragen haben, können Sie uns über{" "}
                     <a href="mailto:info@floralbite.de">info@floralbite.de</a>
@@ -11,14 +11,14 @@ export default function ImpressumPage() {
                 </p>
             </div>
             <div className="impressum">
-                <h1>Impressum</h1>
+                <h2>Impressum</h2>
                 <p>
                     Julia Schreck<br/>
                     FloralBite - Catering<br/>
                     Niebergallstraße 11<br/>
                     64331 Weiterstadt
                 </p>
-                <h2>Kontakt</h2>
+                <h3>Kontakt</h3>
                 <p>
                     Telefon:<br/>
                     E-Mail:{" "}<a href="mailto:info@floralbite.de">info@floralbite.de</a>
