9a19b61ef658dcf055c78efacfe5ab6f6478c4bb
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-16 21:21:55 +0200

    Animation von sich umdrehenden Karten.

--- frontend/src/pages/OfferPage.tsx
+++ frontend/src/pages/OfferPage.tsx
@@ -5,7 +5,7 @@ export default function OfferPage() {
     return (
         <>
             <h2>Das Angebot von FloralBite</h2>
-            <Grid container spacing={3.5} sx={{py: 0.5}}>
+            <Grid container spacing={3} sx={{py: 0.5}}>
                 <Grid size={{xs: 6, sm: 6, md: 4, lg: 2}}><OfferCard type={"cold"}/></Grid>
                 <Grid size={{xs: 6, sm: 6, md: 4, lg: 2}}><OfferCard type={"menu"}/></Grid>
                 <Grid size={{xs: 6, sm: 6, md: 4, lg: 2}}><OfferCard type={"sweet"}/></Grid>
