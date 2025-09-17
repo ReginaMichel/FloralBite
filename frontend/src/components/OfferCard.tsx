271ac2952c3e371fb67996f770e4b6836bb40b99
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-17 10:07:35 +0200

    Unterscheidung zwischen Antippen auf Touch-Geräten und Hovern bei Desktop-Geräten.

--- frontend/src/components/OfferCard.tsx
+++ frontend/src/components/OfferCard.tsx
@@ -1,5 +1,7 @@
 import type {OfferCategory} from "../models/OfferCategory.ts";
 import {Box} from "@mui/material";
+import {useIsTouchDevice} from "../utils/useIsTouchDevice.ts";
+import {useState} from "react";
 
 type OfferCardProps = {
     type: OfferCategory;
@@ -23,8 +25,21 @@ export default function OfferCard(props: Readonly<OfferCardProps>) {
     }
     const imageUrl = "/assets/" + props.type + ".webp";
 
+    // Abfrage, ob das Gerät ein Touch- oder Mausgerät ist:
+    const isTouchDevice = useIsTouchDevice();
+    // Zustand der Karte:
+    const [flipped, setFlipped] = useState(false);
+    // Bei Touch-Geräten, soll ein Click die Karte umdrehen:
+    const handleClick = () => {
+        if (isTouchDevice) {
+            setFlipped((prev) => !prev);
+        }
+    };
+
     return (
         <Box
+            // Bei Touch-Geräten ist klicken möglich:
+            onClick={handleClick}
             sx={{
                 position: "relative",
                 width: "100%",
@@ -32,11 +47,12 @@ export default function OfferCard(props: Readonly<OfferCardProps>) {
                 boxSizing: "border-box",
                 borderRadius: "0.4rem",
                 overflow: "hidden",
+                // Hover-Effekt nur bei Maus-Geräten. Überschreibt die opacity, die durch "flipped" angegeben wird:
                 '&:hover .front': {
-                    opacity: 0,
+                    opacity: isTouchDevice ? undefined : 0,
                 },
                 '&:hover .back': {
-                    opacity: 1,
+                    opacity: isTouchDevice ? undefined : 1,
                 },
             }}
         >
@@ -52,9 +68,10 @@ export default function OfferCard(props: Readonly<OfferCardProps>) {
                     alignItems: "center",
                     justifyContent: "center",
                     borderRadius: "0.4rem",
-                    transition: "opacity 0.5s ease-in-out",
-                    opacity: 1,
-                    zIndex: 2,
+                    transition: "opacity 0.4s ease-in-out",
+                    // Opacity und z-Position kann auch durch Anklicken auf einem Smartphone geändert werden
+                    opacity: flipped ? 0 : 1,
+                    zIndex: flipped ? 1 : 2,
                 }}
             >
                 <h3 style={{ margin: 0, padding: "0 8px", textAlign: "center" }}>{title}</h3>
@@ -67,9 +84,10 @@ export default function OfferCard(props: Readonly<OfferCardProps>) {
                     position: "absolute",
                     width: "100%",
                     height: "100%",
-                    opacity: 0,
-                    transition: "opacity 0.5s ease-in-out",
-                    zIndex: 1,
+                    transition: "opacity 0.4s ease-in-out",
+                    // Opacity und z-Position kann auch durch Anklicken auf einem Smartphone geändert werden
+                    opacity: flipped ? 1 : 0,
+                    zIndex: flipped ? 2 : 1,
                 }}
             >
                 <img
