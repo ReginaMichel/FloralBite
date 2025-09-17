247da4080b4b10282494ed2b376346feaa609df1
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-17 11:58:45 +0200

    Angebotskategorien als Array, damit ich darüber iterieren kann.

--- frontend/src/models/OfferCategory.ts
+++ frontend/src/models/OfferCategory.ts
@@ -1,1 +1,12 @@
 export type OfferCategory = "cold" | "menu" | "sweet" | "savory" | "special" | "party";
+
+// Um darüber iterieren zu können, brauche ich es als Array. Da Typen nur zur Compile-Zeit, aber nicht zur Laufzeit
+// existieren.
+export const offerCategories: OfferCategory[] = [
+    "cold",
+    "menu",
+    "sweet",
+    "savory",
+    "special",
+    "party",
+];
