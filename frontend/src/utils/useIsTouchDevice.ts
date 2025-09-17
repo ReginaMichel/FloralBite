2bc407cc4b0f28de8a92af2acaba936e2ca8fcb1
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-17 09:43:09 +0200

    Korrektur, damit React es als Hook erkennt.

--- frontend/src/utils/useIsTouchDevice.ts
+++ frontend/src/utils/useIsTouchDevice.ts
@@ -1,12 +1,14 @@
 import {useEffect, useState} from "react";
 
-export default function CheckIfTouchDevice() {
+// Der Name muss "use" am Anfang haben, damit React es als Hook erkennt.
+// Hooks sind spezielle Funktionen von React mit denen man auf Zustand, Lifecycle, Context etc. zugreifen kann.
+export function useIsTouchDevice() {
     const [isTouchDevice, setIsTouchDevice] = useState(false);
 
     useEffect(() => {
         const checkTouch = () => {
             // MediaQuery, ob das Gerät über Maus-Hover verfügt oder nicht
-            setIsTouchDevice(window.matchMedia('(hover:none)').matches);
+            setIsTouchDevice(window.matchMedia('(hover: none)').matches);
         };
         // Beim Start
         checkTouch();
