e87ec2789b888334be9e1d41f31c7f27474d2756
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-18 14:43:44 +0200

    Ansicht scrollt hoch zur Detailansicht, wenn sie geöffnet wird.

--- frontend/src/pages/OfferPage.tsx
+++ frontend/src/pages/OfferPage.tsx
@@ -1,7 +1,7 @@
 import OfferCard from "../components/OfferCard.tsx";
 import OfferDetail from "../components/OfferDetail.tsx";
 import {Grid} from "@mui/material";
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import DesignBar from "../components/DesignBar.tsx";
 import {offerCategories, type OfferCategory} from "../models/OfferCategory.ts";
 
@@ -9,11 +9,23 @@ export default function OfferPage() {
 
     const [selected, setSelected] = useState<OfferCategory | undefined>(undefined);
 
+    // Referenzpunkt für flexibles Scrollen zur Detailansicht
+    const detailRef = useRef<HTMLDivElement>(null);
+    // Jedes Mal, wenn die Detailansicht geöffnet wird, springt die Ansicht hoch zur Detailansicht
+    useEffect(() => {
+        if (selected !== undefined) {
+            detailRef.current?.scrollIntoView({ behavior: 'smooth' });
+        }
+    }, [selected]);
+
     return (
         <>
             {selected === undefined ? <h2>Das Angebot von FloralBite</h2>
                 : <>
-                    <OfferDetail type={selected}/>
+                    {/* Referenzpunkt für das Scrollen zur Detailansicht */}
+                    <div ref={detailRef}>
+                        <OfferDetail type={selected}/>
+                    </div>
                     <DesignBar/>
                     <h2>Weitere Angebote</h2>
                 </>}
