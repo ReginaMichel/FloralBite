85f7a71a7251fb47e679ca825bcc3ecb6b616e0b
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-18 12:11:17 +0200

    Beim Hovern False-Positive Gefahr zu Desktop-Geräten mit schmalen Fenstern < 600 und bei Smartphones oder Tablets im Querformat, die Chrome für Android nutzen, verlagert.

--- frontend/src/utils/useIsTouchDevice.ts
+++ frontend/src/utils/useIsTouchDevice.ts
@@ -8,19 +8,16 @@ export function useIsTouchDevice() {
     useEffect(() => {
         // window ist eine Abfrage an die Browser-API
         const checkTouch = () => {
-            // doppelter Check um auf Nummer sicher zu gehen, dass sinnvolle Ergebnisse zurückkommen.
+            // mehrfacher Check um auf Nummer sicher zu gehen, dass sinnvolle Ergebnisse zurückkommen.
             // 'ontouchstart': check für Touchfähigkeit
             // TouchPoints: 0 kein Touchgerät, 1 einfaches Touchgerät, 5 oder 10 Multitouch wie Smartphone oder Tablet
-            // window.matchMedia('(hover: none)').matches ist eine MediaQuery an die Browser-API, die die Fähigkeit zu
-            // hovern abfragt.
-            // Letzteres wurde angefügt, da Desktop-Geräte, an denen potentiell eine Toucheingabe angefügt werden kann
-            // bei ersterem false-positive werden.
-            // Die ersten beiden Abfragen sind da, weil die Browser von modernen Android-Geräten gerne mal
-            // false positive auf die Hover-Fähigkeit antworten, weil theoretisch Display-Stifte angeschlossen werden
-            // könnten.
+            // window.matchMedia('(hover: none)').matches ist eine MediaQuery an die Browser-API über Hover-Fähigkeit
+            // pointer gibt coarse bei smartphones und tablets an, allerdings auch bei manchen Hybridgeräten
+            // innerWidth < xxx ist dabei, weil Chrome auf Android gerne falsche Dinge meldet
+            // innerWidth < 850 oder 600 je nachdem wie man Smartphones im Querformat werten möchte.
             const hasTouchSupport =
-                (('ontouchstart' in window || navigator.maxTouchPoints > 0) &&
-                    window.matchMedia('(hover: none)').matches);
+                ('ontouchstart' in window || navigator.maxTouchPoints > 0) &&
+                (window.matchMedia('(pointer: coarse)').matches || window.innerWidth < 600 || window.matchMedia('(hover: none)').matches);
             setIsTouchDevice(hasTouchSupport);
         };
         // Beim Start
