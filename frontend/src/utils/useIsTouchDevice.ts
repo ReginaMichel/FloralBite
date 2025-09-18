e49d4193d80d08e0618bca40166e3308bfe1cd3d
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-18 10:17:59 +0200

    Versuch Desktop-Geräte wieder als nicht-Touchgerät zu erkennen. (:

--- frontend/src/utils/useIsTouchDevice.ts
+++ frontend/src/utils/useIsTouchDevice.ts
@@ -11,7 +11,16 @@ export function useIsTouchDevice() {
             // doppelter Check um auf Nummer sicher zu gehen, dass sinnvolle Ergebnisse zurückkommen.
             // 'ontouchstart': check für Touchfähigkeit
             // TouchPoints: 0 kein Touchgerät, 1 einfaches Touchgerät, 5 oder 10 Multitouch wie Smartphone oder Tablet
-            const hasTouchSupport = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+            // window.matchMedia('(hover: none)').matches ist eine MediaQuery an die Browser-API, die die Fähigkeit zu
+            // hovern abfragt.
+            // Letzteres wurde angefügt, da Desktop-Geräte, an denen potentiell eine Toucheingabe angefügt werden kann
+            // bei ersterem false-positive werden.
+            // Die ersten beiden Abfragen sind da, weil die Browser von modernen Android-Geräten gerne mal
+            // false positive auf die Hover-Fähigkeit antworten, weil theoretisch Display-Stifte angeschlossen werden
+            // könnten.
+            const hasTouchSupport =
+                (('ontouchstart' in window || navigator.maxTouchPoints > 0) &&
+                    window.matchMedia('(hover: none)').matches);
             setIsTouchDevice(hasTouchSupport);
         };
         // Beim Start
