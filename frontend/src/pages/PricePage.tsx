934b7b1746ad81e21a20dd96f0e92530fc264b93
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-18 13:27:55 +0200

    minor styling adjustment

--- frontend/src/pages/PricePage.tsx
+++ frontend/src/pages/PricePage.tsx
@@ -4,15 +4,17 @@ import PriceList from "../components/PriceList.tsx";
 import DesignBar from "../components/DesignBar.tsx";
 import {Box} from "@mui/material";
 
-export default function PricesPage() {
+export default function PricePage() {
     return (
         <>
             <h2>Angebote und Preise</h2>
-            <p>Hier findest du unsere aktuellen Angebote und Preise. Für individuelle Angebote, kannst du dich auch
+            <p>Hier findest du die aktuellen Angebote und Preise. Für individuelle Angebote, kannst du dich auch
                 gerne bei mir melden:</p>
-            <button className={"contactForm"}>
-                <Link to={"/kontakt"}>Individuelle Anfrage</Link>
-            </button>
+            <Box sx={{textAlign: "center", paddingBottom: 1}}>
+                <button className={"contactForm"}>
+                    <Link to={"/kontakt"}>Individuelle Anfrage</Link>
+                </button>
+            </Box>
             <DesignBar/>
             {offerCategories.map((cat) => <PriceList type={cat}/>)}
             <Box sx={{textAlign: "center", py: {xs: '4%', sm: '3%'}}}>
