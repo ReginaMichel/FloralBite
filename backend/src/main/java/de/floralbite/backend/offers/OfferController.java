64121d7dee51529a133280da2a7c9239ca113034
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-19 15:09:22 +0200

    Post und GetAll Endpunkte für Einzelangebote und Menüangebote.

---
+++ backend/src/main/java/de/floralbite/backend/offers/OfferController.java
@@ -0,0 +1,46 @@
+package de.floralbite.backend.offers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/offers")
+public class OfferController {
+
+    private final OfferService offerService;
+
+    public OfferController(OfferService offerService) {
+        this.offerService = offerService;
+    }
+
+    @PostMapping("/single")
+    public ResponseEntity<SingleOffer> addSingleOffer (@RequestBody SingleOfferDTO singleOfferDto) {
+        SingleOffer savedOffer = offerService.addSingleOffer(singleOfferDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedOffer);
+    }
+    @PostMapping("/menu")
+    public ResponseEntity<MenuOffer> addMenuOffer (@RequestBody MenuOfferDTO menuOfferDto) {
+        MenuOffer savedOffer = offerService.addMenuOffer(menuOfferDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedOffer);
+    }
+
+    @GetMapping("/single")
+    public ResponseEntity<List<SingleOffer>> getAllSingleOffers() {
+        List<SingleOffer> allOffers = offerService.getAllSingleOffers();
+        if (allOffers.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(allOffers);
+    }
+    @GetMapping("/menu")
+    public ResponseEntity<List<MenuOffer>> getAllMenuOffers() {
+        List<MenuOffer> allOffers = offerService.getAllMenuOffers();
+        if (allOffers.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(allOffers);
+    }
+}
