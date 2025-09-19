61ce34e80a3e8f9f89e95f6d671b4505031557d4
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-19 14:48:12 +0200

    Add and getAll methods for SingleOffers and MenuOffers.

--- backend/src/main/java/de/floralbite/backend/offers/OfferService.java
+++ backend/src/main/java/de/floralbite/backend/offers/OfferService.java
@@ -3,6 +3,8 @@ package de.floralbite.backend.offers;
 import de.floralbite.backend.messages.IdService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class OfferService {
 
@@ -14,5 +16,23 @@ public class OfferService {
         this.singleOfferRepo = singleOfferRepo;
         this.menuOfferRepo = menuOfferRepo;
         this.idService = idService;
+    }
+
+    public SingleOffer addSingleOffer(SingleOfferDTO offerDTO) {
+        String id = idService.newId();
+        SingleOffer offer = new SingleOffer(id, offerDTO.name(), offerDTO.category(), offerDTO.description());
+        return singleOfferRepo.save(offer);
+    }
+    public MenuOffer addMenuOffer(MenuOfferDTO offerDTO) {
+        String id = idService.newId();
+        MenuOffer offer = new MenuOffer(id, offerDTO.name(), offerDTO.category(), offerDTO.content(), offerDTO.price());
+        return menuOfferRepo.save(offer);
+    }
+
+    public List<SingleOffer> getAllSingleOffers() {
+        return singleOfferRepo.findAll();
+    }
+    public List<MenuOffer> getAllMenuOffers() {
+        return menuOfferRepo.findAll();
     }
 }
