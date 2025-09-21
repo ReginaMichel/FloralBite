ba1f6e81cff5db05787f3f7eb6d158909f86de8b
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-09-21 16:32:41 +0200

    Verlinkung der Preisliste :)

--- frontend/src/components/OfferDetail.tsx
+++ frontend/src/components/OfferDetail.tsx
@@ -1,7 +1,6 @@
 import {type OfferCategory, offerCategoryTitles, offerCategoryDescriptions} from "../models/OfferCategory.ts";
 import Gallery from "./Gallery.tsx";
-import {Link} from "react-router-dom";
-import {Stack} from "@mui/material";
+import ToPricePage from "./ToPricePage.tsx";
 
 type OfferDetailProps = {
     type: OfferCategory;
@@ -18,20 +17,7 @@ export default function OfferDetail(props: Readonly<OfferDetailProps>) {
             <p>{description}</p>
             <h3>Beispiele</h3>
             <Gallery type={props.type} title={""}/>
-            <h3>Angebot und Preise</h3>
-            <p>Lust auf mehr? Konkrete Angebote und Preise findest du in der Angebotsübersicht oder melde dich bei mir
-            für ein individuelles Angebot nach deinen Wünschen. Ich freue mich, von dir zu hören.</p>
-            {/* Button-Komponente vom Typ Link ist besser für die Barrierefreiheit als Buttons und Links zu
-            verschachteln. */}
-            <Stack spacing={3} direction="row" sx={{py: 1.5}}>
-                {/* Bei Bedarf Zeilenumbruch mit automatischem Bindestrich. */}
-                <button className={"contactForm wordbreak"}>
-                    <Link to={"/preise"} lang={"de"}>Angebotsübersicht</Link>
-                </button>
-                <button className={"contactForm"}>
-                    <Link to={"/kontakt"}>Individuelle Anfrage</Link>
-                </button>
-            </Stack>
+            <ToPricePage/>
         </>
     )
 }
